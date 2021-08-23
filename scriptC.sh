61eb4a187da9bed48972313c7aab6ac469960a26
ScarMuffin <girsay@mail.ru>
2021-08-24 01:07:50 +0300

    Fix

--- scriptC.sh
+++ scriptC.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /tmp/investigation #Creating investigation folder if its not exits 
 for pid in $(ps -C java -o pid=); do #Starting the loop that search java PIDs
-   ls -l /proc/$pid/fd > /tmp/investigation/access_$pid.txt #Log that PIDs to access_$pid.txt
+   ls -l /proc/$pid/fd > /tmp/investigation/access_$pid.txt #Log that PIDs file accesses to access_$pid.txt
 done #The end of the loop
